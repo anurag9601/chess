c7a4abdc93dcb5852035e8d85f001cad87fd9d6e
anurag9601 <anuragmishrap13@gmail.com>
2025-10-22 12:08:57 +0530

    Created mongodb connect function

---
+++ src/mongodb/connectDB.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface I {
+    isConnected?: boolean;
+    connectionId?: number;
+}
+
+const url = process.env.NEXT_PUBLIC_MONGODB_URL || "";
+
+const connection: I = {};
+
+export async function connectMongoDB() {
+    if (connection.isConnected && connection.connectionId) {
+        console.log("MongoDB is already connected");
+        return;
+    };
+
+    try {
+        const db = await mongoose.connect(url as string);
+
+        connection.isConnected = true;
+        connection.connectionId = db.connections[0].readyState;
+
+        console.log("MongoDB connected successfully 🚀.");
+
+    } catch (error) {
+        console.log("Error while connecting MongoDB", error);
+
+        process.exit(1);
+    }
+}
