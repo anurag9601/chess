324cdf0f795dcaf2d859ff7fc41e2295870908f8
anurag9601 <anuragmishrap13@gmail.com>
2026-01-01 15:52:18 +0530

    implemented play online request send accept tabs

--- src/app/(frontend)/play/page.tsx
+++ src/app/(frontend)/play/page.tsx
@@ -1,16 +1,18 @@
-import ChessBoard from '@/components/chess-board/chessBoard'
-import Players from '@/components/players/Players'
-import Settings from '@/components/settings/Settings'
-import React from 'react'
+import ChessBoard from "@/components/chess-board/chessBoard";
+import FriendsListing from "@/components/friends-listing/friendsListing";
+import Players from "@/components/players/Players";
+import Settings from "@/components/settings/Settings";
+import React from "react";
 
 const page = () => {
   return (
-    <div className="chessPlayContainer min-h-screen min-w-screen flex items-center justify-between px-[10%] py-[20px]">
+    <div className="chessPlayContainer min-h-screen min-w-screen flex items-center justify-between px-[5%] py-[20px]">
+      <FriendsListing />
       <Players />
       <ChessBoard />
       <Settings />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
