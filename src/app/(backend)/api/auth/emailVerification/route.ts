55ad406bdb1b47adea9f2bd010228dd847d1a865
anurag9601 <anuragmishrap13@gmail.com>
2026-01-16 22:16:32 +0530

    solved _id token payload error

--- src/app/(backend)/api/auth/emailVerification/route.ts
+++ src/app/(backend)/api/auth/emailVerification/route.ts
@@ -63,6 +63,7 @@ export async function POST(req: NextRequest) {
             );
 
             const tokenPayload: generateJWTDataType = {
+                _id: updatedUser._id,
                 fName: updatedUser.fName,
                 lName: updatedUser.lName,
                 userEmail: updatedUser.userEmail,
